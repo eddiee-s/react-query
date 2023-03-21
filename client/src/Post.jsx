7d562379a847217a6797f424cef9e011fb9e7542
eddiee-s <edis.pajaziti@gmail.com>
2023-03-21 15:44:39 +0100

    geting name for user done for single post

--- client/src/Post.jsx
+++ client/src/Post.jsx
@@ -1,10 +1,17 @@
 import { useQuery } from "@tanstack/react-query"
 import { getPost } from "./api/posts"
+import { getUser } from "./api/user"
 
 const Post = ({ id }) => {
     const postQuery = useQuery({
         queryKey: ['posts', id],
         queryFn: () => getPost(id)
+    })
+
+    const userQuery = useQuery({
+        queryKey: ['users', postQuery?.data?.userId],
+        enabled: postQuery.data?.userId != null, // if there is data fetched from postQuery
+        queryFn: () => getUser(postQuery.data.userId)
     })
 
     if (postQuery.status === 'loading') return <h1>Loading...</h1>
@@ -16,7 +23,13 @@ const Post = ({ id }) => {
         <>
             <h1>{postQuery.data.title}</h1>
             <small>
-                {postQuery.data.userId}
+                {/* {postQuery.data.userId}  */}
+                {userQuery.isLoading
+                    ? "Loading..."
+                    : userQuery.isError
+                        ? "Error loading user"
+                        : userQuery.data.name
+                }
             </small>
             <p>
                 {postQuery.data.body}
